2853f920e2a7d177e1a6eaf3ade13679643f839b
MyViboon <95564348+MyViboon@users.noreply.github.com>
2023-01-05 17:52:45 +0700

    add navbar

--- dashboard/src/pages/home/Home.jsx
+++ dashboard/src/pages/home/Home.jsx
@@ -1,10 +1,20 @@
 import "./home.scss";
 import Sidebar from "../../components/sidebar/Sidebar"
+import Navbar from "../../components/navbar/Navbar";
+import Wigget from "../../components/wigget/Wigget";
 
 const Home = () => {
   return <div className="home">
     <Sidebar/>
-    <div className="homeContainer">Container</div>
+    <div className="homeContainer">
+      <Navbar/>
+      <div className="wiggets">
+        <Wigget type="user"/>
+        <Wigget type="order"/>
+        <Wigget type="earning"/>
+        <Wigget type="balance"/>
+      </div>
+    </div>
   </div>;
 };
 
