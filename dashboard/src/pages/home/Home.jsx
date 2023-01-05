0f11b36b159a48f7d205a89ff7d19c64d512e50f
MyViboon <95564348+MyViboon@users.noreply.github.com>
2023-01-05 21:53:25 +0700

    add chart

--- dashboard/src/pages/home/Home.jsx
+++ dashboard/src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import "./home.scss";
 import Sidebar from "../../components/sidebar/Sidebar"
 import Navbar from "../../components/navbar/Navbar";
 import Wigget from "../../components/wigget/Wigget";
+import Featured from "../../components/featured/Featured";
+import Chart from "../../components/chart/Chart";
 
 const Home = () => {
   return <div className="home">
@@ -13,6 +15,10 @@ const Home = () => {
         <Wigget type="order"/>
         <Wigget type="earning"/>
         <Wigget type="balance"/>
+      </div>
+      <div className="charts">
+        <Featured/>
+        <Chart/>
       </div>
     </div>
   </div>;
